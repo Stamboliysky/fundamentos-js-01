872ca3ae370cd3b49a569a3087030c18964066f3
Stamboliysky <jordanspassoff@gmail.com>
2023-10-24 22:01:03 +0200

    Anadir ejersicio 01

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,78 @@
+// 1.
+let nombre = 'Jordan'
+// 2.
+const edad = 61
+// 3.
+const esEstudiante = true
+// 4.
+const mensaje = 'Hola mi nombre es  ' + nombre   + '  y  tengo ' + edad + ' años. Soy estudiante: ' + esEstudiante +'.'
+// 5.
+const num1 = 10
+// 6.
+const num2 = 5
+// 7.
+const suma = num1 + num2
+// 8.
+const resta = num1 - num2
+// 9.
+const multiplicacion = num1 * num2 
+// 10.
+const divicion = num1 / num2 
+// 11.
+console.log (mensaje)
+console.log (`Suma :`,suma)
+console.log (' Resta',resta)
+console.log ('multiplicacion')
+console.log ('Division')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
